43289ab7ce275499676f3161a5dcc72b4bc7da31
haojun.sun <coolboss1@sina.com>
2016-11-24 16:12:56 +0800

    搭建 后台

--- LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/HomeController.cs
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace LittleBackstage.Web.Areas.Admin.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         // GET: Admin/Home
