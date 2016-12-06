f57af08c2d3e9fdf91295b3acd79c3fe78436a58
haojun.sun <coolboss1@sina.com>
2016-12-06 14:37:24 +0800

    添加 空白页面 与 action

--- LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
@@ -13,10 +13,12 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
     {
         private readonly IManagerService _managerService;
         private readonly IHelperServices _helperServices;
-        public UserController(IManagerService managerService, IHelperServices helperServices)
+        private readonly IRoleService _roleService;
+        public UserController(IManagerService managerService, IHelperServices helperServices,IRoleService roleService)
         {
             _managerService = managerService;
             _helperServices = helperServices;
+            _roleService = roleService;
         }
         // GET: Admin/User
         public ActionResult Index()
@@ -24,6 +26,7 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
             return View();
         }
 
+        /*管理员*/
         public ActionResult AddManager()
         {
             return View();
@@ -32,11 +35,38 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
         {
             return View();
         }
-
         public ActionResult ManagerList(string key, int page, int size)
         {
             return View();
         }
 
+        /*角色*/
+        public ActionResult AddRole()
+        {
+            return View();
+        }
+        public ActionResult EditRole(int id)
+        {
+            return View();
+        }
+        public ActionResult RoleList(string key, int page, int size)
+        {
+            return View();
+        }
+
+        /*用户*/
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+        public ActionResult EditUser(int id)
+        {
+            return View();
+        }
+
+        public ActionResult UserList(string key, int page, int size)
+        {
+            return View();
+        }
     }
 }
