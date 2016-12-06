f29f81de58c809ca8fc2fc0a470f98c366e43f3b
haojun.sun <coolboss1@sina.com>
2016-12-06 14:32:00 +0800

    菜单功能 权限判断 管理员功能 列表 添加（选角色） 删除

--- LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
@@ -3,15 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LittleBackstage.Core.Services;
+using LittleBackstage.Infrastructure.Services;
 
 namespace LittleBackstage.Web.Areas.Admin.Controllers
 {
+    //[Authorize]
     public class UserController : Controller
     {
+        private readonly IManagerService _managerService;
+        private readonly IHelperServices _helperServices;
+        public UserController(IManagerService managerService, IHelperServices helperServices)
+        {
+            _managerService = managerService;
+            _helperServices = helperServices;
+        }
         // GET: Admin/User
         public ActionResult Index()
         {
             return View();
         }
+
+        public ActionResult AddManager()
+        {
+            return View();
+        }
+        public ActionResult EditManager(int id)
+        {
+            return View();
+        }
+
+        public ActionResult ManagerList(string key, int page, int size)
+        {
+            return View();
+        }
+
     }
 }
