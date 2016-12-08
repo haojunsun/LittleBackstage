9930361afa941c3bd44923c4f9c26bc8987d81a1
haojun.sun <coolboss1@sina.com>
2016-12-08 17:20:57 +0800

    管理员 添加 编辑 删除

--- LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LittleBackstage.Core.Models;
 using LittleBackstage.Core.Services;
 using LittleBackstage.Infrastructure.Services;
 using LittleBackstage.Web.Areas.Admin.Models;
@@ -32,16 +33,135 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
         /*管理员*/
         public ActionResult AddManager()
         {
+            TreeBindRole(0, 0);
             return View();
         }
+
+        /// <summary>
+        ///  绑定类别
+        /// </summary>
+        /// <param name="pid"></param>
+        /// <param name="type"></param>
+        /// <param name="id"></param>
+        private void TreeBindRole(int type, int id)
+        {
+            var list = new List<Role>();
+
+            list = _roleService.List().Where(x => x.RoleType == type).ToList();
+
+            var selectList = new List<SelectListItem>();
+
+            foreach (var r in list)
+            {
+                if (r.RoleId == id)
+                {
+                    selectList.Add(new SelectListItem()
+                    {
+                        Value = r.RoleId.ToString(),
+                        Text = r.RoleName,
+                        Selected = true
+                    });
+                }
+                else
+                {
+                    selectList.Add(new SelectListItem()
+                    {
+                        Value = r.RoleId.ToString(),
+                        Text = r.RoleName,
+                    });
+                }
+            }
+            ViewBag.RoleSelect = selectList;
+        }
+
+        [HttpPost]
+        public ActionResult AddManager(string userName, string passWord, int? IsExamineRadios = 0, int? role = 0)
+        {
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(passWord))
+            {
+                return Content("<script>alert('创建失败,登录帐号或密码不能为空!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+            if (role == 0)
+            {
+                return Content("<script>alert('创建失败,未选择角色!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+            if (_managerService.FindByUserName(userName))
+            {
+                return Content("<script>alert('创建失败,登录帐号已存在!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+
+            var manager = new Manager();
+            manager.IsExamine = (int)IsExamineRadios;
+            manager.IsEnable = (int)IsExamineRadios;
+            manager.PassWord = _helperServices.MD5CSP(passWord);
+            manager.Register = DateTime.Now;
+            manager.UserName = userName;
+            manager.Role = _roleService.Get((int)role);
+
+            _managerService.Add(manager);
+            return Content("<script>alert('创建成功!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+        }
+
         public ActionResult EditManager(int id)
         {
-            return View();
+            var model = _managerService.Get(id);
+            if (model == null)
+            {
+                return Content("<script>alert('参数错误,返回列表!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+            TreeBindRole(0, model.Role.RoleId);
+            return View(model);
         }
+
+        [HttpPost]
+        public ActionResult EditManager(Manager m, string passWord, int? IsExamineRadios = 0)
+        {
+            if (string.IsNullOrEmpty(m.UserName))
+            {
+                return Content("<script>alert('编辑失败,登录帐号不能为空!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+
+            var old = _managerService.Get(m.ManagerId);
+            if (old == null)
+            {
+                return Content("<script>alert('编辑失败,数据错误!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+
+            if (!string.IsNullOrEmpty(passWord))
+            {
+                old.PassWord = _helperServices.MD5CSP(passWord);
+            }
+            old.IsEnable = (int)IsExamineRadios;
+            old.IsExamine = (int)IsExamineRadios;
+            if (m.Role.RoleId != 0)
+            {
+                old.Role = _roleService.Get(m.Role.RoleId);
+            }
+            old.UserName = m.UserName;
+            _managerService.Update(old);
+            return Content("<script>alert('编辑成功!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+        }
+
         public ActionResult ManagerList()
         {
             var list = _managerService.List().Where(x => x.UserName != "admin").OrderByDescending(x => x.Register);
             return View(list.ToList());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult DelManager(int id)
+        {
+            var m = _managerService.Get(id);
+            if (m == null)
+            {
+                return Content("<script>alert('删除失败,参数错误!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
+            }
+            _managerService.Delete(id);
+            return Content("<script>alert('删除成功!');window.location.href='" + Url.Action("ManagerList") + "';</script>");
         }
 
         /*角色*/
