e1682dfc22973315e843e1ca07929d38b939e2c6
haojun.sun <coolboss1@sina.com>
2016-12-28 16:12:42 +0800

    添加条目管理

---
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/EntryTableController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LittleBackstage.Core.Models;
+using LittleBackstage.Core.Services;
+using LittleBackstage.Infrastructure.Services;
+using LittleBackstage.Web.Helpers;
+
+namespace LittleBackstage.Web.Areas.Admin.Controllers
+{
+
+    /// <summary>
+    /// 条目
+    /// </summary>
+    public class EntryTableController : Controller
+    {
+        private readonly IManagerService _managerService;
+        private readonly IHelperServices _helperServices;
+        private readonly IRoleService _roleService;
+        private readonly IUserService _userService;
+        private readonly ICategoryService _categoryService;
+        private readonly ILogService _logService;
+        private readonly ICategoryFieldService _categoryFieldService;
+
+        public EntryTableController(IManagerService managerService,
+            IHelperServices helperServices,
+            IRoleService roleService,
+            IUserService userService,
+            ICategoryService categoryService,
+            ILogService logService, ICategoryFieldService categoryFieldService)
+        {
+            _managerService = managerService;
+            _helperServices = helperServices;
+            _roleService = roleService;
+            _userService = userService;
+            _categoryService = categoryService;
+            _logService = logService;
+            _categoryFieldService = categoryFieldService;
+        }
+
+        // GET: Admin/EntryTable
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult EntryList(int id)
+        {
+            return View();
+        }
+
+    }
+}
