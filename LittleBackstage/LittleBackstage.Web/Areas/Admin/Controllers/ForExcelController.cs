efc6dffe4a237fbde83a2a0dc0f9e69f2a423e3c
haojun.sun <coolboss1@sina.com>
2017-03-15 14:22:34 +0800

    分页

--- LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/ForExcelController.cs
+++ LittleBackstage/LittleBackstage.Web/Areas/Admin/Controllers/ForExcelController.cs
@@ -62,24 +62,75 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
         /// <param name="pageIndex"></param>
         /// <returns></returns>
         [HttpPost]
-        public ActionResult SeniorSearch(string key, string fl, int pageSize, int pageIndex)
+        public ActionResult SeniorSearch(int pageSize, int pageIndex, int id = 0)
         {
             var totalCount = 0;
-            if (string.IsNullOrEmpty(key) && string.IsNullOrEmpty(fl))
+            var c = _categoryService.List().First(x => x.IsCreateTable == 1 && x.CategoryId == id);
+            if (c != null)
             {
-                var all = _forExcelService.List(pageIndex, pageSize, ref totalCount);
-                var jsonData = new JsonData();
-                jsonData.totalCount = totalCount;
-                jsonData.list = all.ToList();
-                return Json(jsonData, JsonRequestBehavior.DenyGet);
-            }
+                var table = new DataTable();
 
-            var result = _forExcelService.SeniorSearch(key, fl, pageIndex, pageSize, ref totalCount);
-            var jsonData2 = new JsonData();
-            jsonData2.totalCount = totalCount;
-            jsonData2.list = result.ToList();
-            return Json(jsonData2, JsonRequestBehavior.DenyGet);
+                var sql = @"select * from " + c.DataTableName;
+                table = SqlHelper.QueryDataTable(SqlHelper.ConnectionStringLocalTransaction, CommandType.Text, sql, null);
+                totalCount = table.Rows.Count;//数据总数
+                var list = GetPagedTable(table, pageIndex, pageSize);
+                var json = new
+                {
+                    list,
+                    totalCount
+                };
+                return Json(json, JsonRequestBehavior.DenyGet);
+            }
+            return Json(null, JsonRequestBehavior.DenyGet);
         }
+        public DataTable GetPagedTable(DataTable dt, int PageIndex, int PageSize)//PageIndex表示第几页，PageSize表示每页的记录数
+        {
+            if (PageIndex == 0)
+                return dt;//0页代表每页数据，直接返回
+
+            DataTable newdt = dt.Copy();
+            newdt.Clear();//copy dt的框架
+
+            int rowbegin = (PageIndex - 1) * PageSize;
+            int rowend = PageIndex * PageSize;
+
+            if (rowbegin >= dt.Rows.Count)
+                return newdt;//源数据记录数小于等于要显示的记录，直接返回dt
+            if (rowend > dt.Rows.Count)
+                rowend = dt.Rows.Count;
+            for (int i = rowbegin; i <= rowend - 1; i++)
+            {
+                DataRow newdr = newdt.NewRow();
+                DataRow dr = dt.Rows[i];
+                foreach (DataColumn column in dt.Columns)
+                {
+                    newdr[column.ColumnName] = dr[column.ColumnName];
+                }
+                newdt.Rows.Add(newdr);
+            }
+            return newdt;
+        }
+
+        //public ActionResult SeniorSearch(string key, string fl, int pageSize, int pageIndex)
+        //{
+        //    var totalCount = 0;
+        //    if (string.IsNullOrEmpty(key) && string.IsNullOrEmpty(fl))
+        //    {
+        //        var all = _forExcelService.List(pageIndex, pageSize, ref totalCount);
+        //        var jsonData = new JsonData();
+        //        jsonData.totalCount = totalCount;
+        //        jsonData.list = all.ToList();
+        //        return Json(jsonData, JsonRequestBehavior.DenyGet);
+        //    }
+
+        //    var result = _forExcelService.SeniorSearch(key, fl, pageIndex, pageSize, ref totalCount);
+        //  var jsonData2 = new JsonData();
+        //    jsonData2.totalCount = totalCount;
+        //    jsonData2.list = result.ToList();
+        //    return Json(jsonData2, JsonRequestBehavior.DenyGet);
+        //}
+
+
 
         /// <summary>
         /// 详情
@@ -133,121 +184,121 @@ namespace LittleBackstage.Web.Areas.Admin.Controllers
                             if (j == 1)//人工编码
                                 wh1.ArtificialId = dt.Rows[i][j].ToString();
                             if (j == 2)//文件名
-                                wh1.TitleProper = dt.Rows[i][j].ToString().Replace("'","\"").Replace("'","\"");
+                                wh1.TitleProper = dt.Rows[i][j].ToString().Replace("'", "\"").Replace("'", "\"");
                             if (j == 3)//项目名称
-                                wh1.XiangMuMingCheng = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangMuMingCheng = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 4)//一级分类
-                                wh1.FirstLevel = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.FirstLevel = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 5)//二级分类
-                                wh1.SecondLevel = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.SecondLevel = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 6)//项目编码
-                                wh1.XiangMuBianMa = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangMuBianMa = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 7)//子项序号
-                                wh1.ZiXiangXuHao = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ZiXiangXuHao = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 8)//批次
-                                wh1.PiCi = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.PiCi = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 9)//注释
-                                wh1.Annotation = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Annotation = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 10)//项目申请时间
-                                wh1.XiangMuShenQingShiJian = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangMuShenQingShiJian = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 11)//民族
-                                wh1.MinZu = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.MinZu = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 12)//直系
-                                wh1.ZhiXi = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ZhiXi = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 13)//项目简介标准版
-                                wh1.XiangMuJianJieBiaoZhunBan = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangMuJianJieBiaoZhunBan = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 14)//项目简介
-                                wh1.XiangMuJianJie = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangMuJianJie = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 15)//所在区域及其地理环境
-                                wh1.SuoZaiQuYuJiQiDiLiHuanJing = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.SuoZaiQuYuJiQiDiLiHuanJing = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 16)//分布区域
-                                wh1.FenBuQuYu = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.FenBuQuYu = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 17)//历史渊源
-                                wh1.LiShiYuanYuan = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.LiShiYuanYuan = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 18)//基本内容
-                                wh1.JiBenNeiRong = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.JiBenNeiRong = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 19)//相关制品及其作品
-                                wh1.XiangGuanZhiPinJiQiZuoPin = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.XiangGuanZhiPinJiQiZuoPin = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 20)//传承谱系
-                                wh1.ChuanChengPuXi = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ChuanChengPuXi = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 21)//代表性传承人
-                                wh1.DaiBiaoXingChuanChengRen = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DaiBiaoXingChuanChengRen = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 22)//主要特征
-                                wh1.ZhuYaoTeZheng = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ZhuYaoTeZheng = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 23)//重要价值
-                                wh1.ZhongYaoJiaZhi = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ZhongYaoJiaZhi = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 24)//存续状况
-                                wh1.CunXuZhuangKuang = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CunXuZhuangKuang = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 25)//主要传承人
-                                wh1.ZhuYaoChuanChengRen = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.ZhuYaoChuanChengRen = dt.Rows[i][j].ToString().Replace("'", "\"");
                             //if (j == 26)//项目名称 重复
                             //wh1.Type = dt.Rows[i][j].ToString().Replace("'","\"");
                             if (j == 27)//类型-图片数组
-                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 28)
-                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 29)
-                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 30)
-                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 31)
-                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Type = wh1.Type + "," + dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 32)//主要传承人记录类型-视频
-                                wh1.Other1 = dt.Rows[i][j].ToString().Replace("'","\"");//TypeVideo
+                                wh1.Other1 = dt.Rows[i][j].ToString().Replace("'", "\"");//TypeVideo
                             if (j == 33)//省
-                                wh1.CoverageSpatial_Province = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_Province = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 34)//市
-                                wh1.CoverageSpatial_City = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_City = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 35)//区
-                                wh1.CoverageSpatial_County = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_County = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 36)//申报地区或单位
-                                wh1.CoverageSpatial_ShenBaoDiQuHuoDanWei = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_ShenBaoDiQuHuoDanWei = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 37)//保护单位
-                                wh1.CoverageSpatial_BaoHuDanWei = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_BaoHuDanWei = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 38)//生态区项目
-                                wh1.CoverageSpatial_ShengTaiQuXiangMu = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_ShengTaiQuXiangMu = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 39)//生产性保护示范基地
-                                wh1.CoverageSpatial_ShengChanXingBaoHuShiFanJiDi = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.CoverageSpatial_ShengChanXingBaoHuShiFanJiDi = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 40)//备注暂时写 语言
-                                wh1.Other2 = dt.Rows[i][j].ToString().Replace("'","\"");//Yuyan
+                                wh1.Other2 = dt.Rows[i][j].ToString().Replace("'", "\"");//Yuyan
                             if (j == 41)//源资料创建者  名称
-                                wh1.Source_CreatorOfReferences_Name = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Source_CreatorOfReferences_Name = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 42)//责任方式
-                                wh1.Source_CreatorOfReferences_Role = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Source_CreatorOfReferences_Role = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 43)//源资料提供者
-                                wh1.Source_ProviderOfReferences = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Source_ProviderOfReferences = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 44)//源资料典藏单位
-                                wh1.Source_RepositoryName = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.Source_RepositoryName = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 45)//数字化格式
-                                wh1.DigitalObjectInformation_DigitalObjectFormat = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_DigitalObjectFormat = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 46)//大小
-                                wh1.DigitalObjectInformation_Size = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_Size = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 47)//时长
-                                wh1.DigitalObjectInformation_Duration = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_Duration = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 48)//分辨率（解析度）
-                                wh1.DigitalObjectInformation_DigitalSpecification = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_DigitalSpecification = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 49)//音/视频数据码率
-                                wh1.DigitalObjectInformation_AudioVideo = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_AudioVideo = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 50)//音频采样频率
-                                wh1.DigitalObjectInformation_AudioSamplingFrequency = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_AudioSamplingFrequency = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 51)//声道数
-                                wh1.DigitalObjectInformation_NumberOfChannels = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_NumberOfChannels = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 52)//储存地址
-                                wh1.DigitalObjectInformation_FilePath = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_FilePath = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 53)//显示级别
-                                wh1.DigitalObjectInformation_DisplayType = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.DigitalObjectInformation_DisplayType = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 54)//著录者
-                                wh1.RecordingInformation_Recorder = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_Recorder = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 55)//著录时间
-                                wh1.RecordingInformation_RecordingTime = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_RecordingTime = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 56)//审核者
-                                wh1.RecordingInformation_MetadataAuditor = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_MetadataAuditor = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 57)//审核时间
-                                wh1.RecordingInformation_MetadataAuditoTime = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_MetadataAuditoTime = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 58)//管理机构
-                                wh1.RecordingInformation_MetadataManagement = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_MetadataManagement = dt.Rows[i][j].ToString().Replace("'", "\"");
                             if (j == 59)//备注
-                                wh1.RecordingInformation_Note = dt.Rows[i][j].ToString().Replace("'","\"");
+                                wh1.RecordingInformation_Note = dt.Rows[i][j].ToString().Replace("'", "\"");
                         }
                         //遍历excel每行 插入数据
                         var insertSql = @"INSERT INTO ";
